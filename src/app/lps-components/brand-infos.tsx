267b96ded55953c854f4f49f580edd471c4cb8b5
Vinicius Augusto <vinicius92as@gmail.com>
2024-12-03 11:52:57 -0300

    adaptando smartphone

--- src/app/lps-components/brand-infos.tsx
+++ src/app/lps-components/brand-infos.tsx
@@ -4,8 +4,8 @@ import Contato from "./lp-contato";
 
 export default function BrandInfos(data: Data) {
     return (
-        <div className="bg-white h-[490px] flex flex-col place-items-center justify-center">
-                <p className="text-black text-3xl w-4/6 text-center">Na <strong className="font-sariaBlack">TS PNEUS</strong>, 
+        <div className="bg-white h-fit flex flex-col place-items-center justify-center pt-10 pb-10 sm:pt-24 sm:pb-24">
+                <p className="text-black text-2xl sm:text-3xl w-10/12 sm:w-4/6 text-center">Na <strong className="font-sariaBlack">TS PNEUS</strong>, 
                 estamos comprometidos em oferecer<strong className="font-sariaBlack"> os melhores pneus para o seu veículo, </strong> 
                 garantindo <strong className="font-sariaBlack">segurança, desempenho e durabilidade.</strong> Por isso, temos uma ampla seleção de
                  <strong className="font-sariaBlack"> pneus {Utils.toPascalCase(data.brand)} disponíveis à pronta entrega.</strong> Conhecida por sua <strong className="font-sariaBlack">inovação e qualidade, </strong> 
--- src/app/lps-components/lp-banner.tsx
+++ src/app/lps-components/lp-banner.tsx
@@ -3,15 +3,15 @@ import { Info, Data } from "../cidadesInfo"
 export default function LpBanner(data: Data) {
     return (
         <div className="bg-lp-main bg-center bg-no-repeat bg-cover h-fit flex flex-col place-items-center justify-center gap-5 pt-10 pb-5">
-            <div className="flex place-items-center justify-center gap-10">
+            <div className="sm:flex place-items-center justify-center gap-10">
                 <div className="flex flex-col place-items-center">
-                    <h1 className="font-sariaBlack text-6xl">PNEUS {data.brand.toUpperCase()}</h1>
-                    <h2 className="font-sariaBold text-5xl">EM {data.info.city.toUpperCase()}</h2>
+                    <h1 className="font-sariaBlack text-5xl sm:text-6xl">PNEUS {data.brand.toUpperCase()}</h1>
+                    <h2 className="font-sariaBold text-4xl sm:text-5xl">EM {data.info.city.toUpperCase()}</h2>
                 </div>
-                <img src={`landing-pages/${data.brand}/logo.png`} className="w-[359px] h-[66px]" alt="" />
+                <img src={`landing-pages/${data.brand}/logo.png`} className="w-[250px] h-auto pt-2 sm:pt-0 sm:w-[359px] sm:h-[66px]" alt="" />
             </div>
-            <img src={`landing-pages/${data.brand}/carros.png`} className="w-4/6 pt-10" alt="" />
-            <img src={`landing-pages/${data.brand}/pneus.png`} className="w-2/6" alt="" />
+            <img src={`landing-pages/${data.brand}/carros.png`} className="w-4/6 pt-10 hidden sm:inline" alt="" />
+            <img src={`landing-pages/${data.brand}/pneus.png`} className="w-8/12 sm:w-3/12" alt="" />
         </div>
     )
 }
